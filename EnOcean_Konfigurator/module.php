a7d0683acfb89849106bb579354584455765032d
kallipeter <lars.schroeder@einsatzserver.de>
2017-03-05 22:21:49 +0100

    Update module.php

--- EnOcean_Konfigurator/module.php
+++ EnOcean_Konfigurator/module.php
@@ -28,7 +28,7 @@ class EnOcean_Konfigurator extends IPSModule {
 		IPS_SetParent($CatShutterID, $CatEnOceanID); // Kategorie einsortieren unter dem Objekt "EnOcean"
 		
 		$InsShutterID = IPS_CreateInstance("{1463CAE7-C7D5-4623-8539-DD7ADA6E92A9}");
-		$Roomname = ReadPropertyString("Roomname");
+		$Roomname = $this->ReadPropertyString("Roomname");
 		IPS_SetName($InsShutterID, $Roomname); // Instanz benennen
 		IPS_SetInfo($InsShutterID, "Test");
 		IPS_SetPosition($InsShutterID, 1);
