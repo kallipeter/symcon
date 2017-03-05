3e2777ac7638ca8b3f136a775b4da6d69feb3452
kallipeter <lars.schroeder@einsatzserver.de>
2017-03-05 22:49:20 +0100

    Update module.php

--- EnOcean_Konfigurator/module.php
+++ EnOcean_Konfigurator/module.php
@@ -30,17 +30,21 @@ class EnOcean_Konfigurator extends IPSModule {
 		$InsShutterID = IPS_CreateInstance("{1463CAE7-C7D5-4623-8539-DD7ADA6E92A9}");
 		$Eltako_FAM_ID = $this->ReadPropertyString("Eltako_FAM_ID");
 		$Shutter_StartID = $this->ReadPropertyInteger("Shutter_StartID");
-		$Floor = $this->ReadPropertyString("Floor");
-		$Roomname = $this->ReadPropertyString("Roomname");
-		$Location = $this->ReadPropertyString("Location");
-		IPS_SetName($InsShutterID, $Roomname); // Instanz benennen
-		IPS_SetInfo($InsShutterID, "Test");
-		IPS_SetPosition($InsShutterID, 1);
-		IPS_SetParent($InsShutterID, $CatShutterID);
-		//$Roomname = $this->ReadPropertyString("Roomname");
-		//IPS_SetConfiguration($InsShutterID, '{"DeviceID":$this->ReadPropertyInteger("Shutter_StartID"),"ReturnID":$this->ReadPropertyString("Eltako_FAM_ID"),"ButtonMode":1,"EmulateStatus":false}');
-		IPS_SetConfiguration($InsShutterID, '{"DeviceID":'.$Shutter_StartID.',"ReturnID":'.$Eltako_FAM_ID.',"ButtonMode":1,"EmulateStatus":false}');
-		IPS_ApplyChanges($InsShutterID);
+		$create = $this->ReadPropertyString("create_1");
+		$Floor = $this->ReadPropertyString("Floor_1");
+		$Roomname = $this->ReadPropertyString("Roomname_1");
+		$Location = $this->ReadPropertyString("Location_1");
+		if ($create == true)
+		{
+			IPS_SetName($InsShutterID, $Roomname); // Instanz benennen
+			IPS_SetInfo($InsShutterID, "Test");
+			IPS_SetPosition($InsShutterID, 1);
+			IPS_SetParent($InsShutterID, $CatShutterID);
+			//$Roomname = $this->ReadPropertyString("Roomname");
+			//IPS_SetConfiguration($InsShutterID, '{"DeviceID":$this->ReadPropertyInteger("Shutter_StartID"),"ReturnID":$this->ReadPropertyString("Eltako_FAM_ID"),"ButtonMode":1,"EmulateStatus":false}');
+			IPS_SetConfiguration($InsShutterID, '{"DeviceID":'.$Shutter_StartID.',"ReturnID":'.$Eltako_FAM_ID.',"ButtonMode":1,"EmulateStatus":false}');
+			IPS_ApplyChanges($InsShutterID);
+		}
 	}
 	
  }
