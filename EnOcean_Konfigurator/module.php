a0ab0069166366461c7ec029986e828a8ffd4f29
kallipeter <lars.schroeder@einsatzserver.de>
2017-03-05 23:39:02 +0100

    Update module.php

--- EnOcean_Konfigurator/module.php
+++ EnOcean_Konfigurator/module.php
@@ -45,9 +45,9 @@ class EnOcean_Konfigurator extends IPSModule {
 		//$Shutter_StartID = 40;
 		//$Eltako_FAM_ID ="FFAA12";
 		$control_ID = hexdec($Eltako_FAM_ID);
-		$ReturnID = $control_ID+$Shutter_StartID;
-		$control_ID = dechex($control_ID);
-		IPS_SetConfiguration($InsShutterID, '{"DeviceID":'.$Shutter_StartID.',"ReturnID":"'.$control_ID.'","ButtonMode":1,"EmulateStatus":false}');
+		$control_ID = $control_ID+$Shutter_StartID;
+		$ReturnID = dechex($control_ID);
+		IPS_SetConfiguration($InsShutterID, '{"DeviceID":'.$Shutter_StartID.',"ReturnID":"'.$ReturnID.'","ButtonMode":1,"EmulateStatus":false}');
 		IPS_ApplyChanges($InsShutterID);
 	}
 	
